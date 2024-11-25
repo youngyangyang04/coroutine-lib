4d35fcb9eb7935aa0b4f35cbb6b236afef7f7be4
lhs <1135282770@qq.com>
2024-11-25 18:14:13 +0800

    修改了6hook中main.cpp的测试部分

--- fiber_lib/6hook/main.cpp
+++ fiber_lib/6hook/main.cpp
@@ -1,60 +1,132 @@
 #include "ioscheduler.h"
-#include <iostream>
+#include "hook.h"
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <cstring>
-#include <netinet/in.h>
 #include <fcntl.h>
-#include "hook.h"
+#include <iostream>
+#include <stack>
+#include <cstring>
+#include <chrono>
+#include <thread>
 
-using namespace sylar;
+static int sock_listen_fd = -1;
 
-void func()
+void test_accept();
+void error(const char *msg)
 {
-
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
-    
-    sockaddr_in server;
-    server.sin_family = AF_INET;
-    server.sin_port = htons(80);  // HTTP 标准端口
-    server.sin_addr.s_addr = inet_addr("103.235.46.96");
-
-    int rt = connect(sock, (struct sockaddr *)&server, sizeof(server));
-    if(rt)
-    {
-    	std::cout << "connect() failed:" << strerror(errno) << std::endl;
-    	return;
-    }
-    std::cout << "connected\n";
-
-    const char data[] = "GET / HTTP/1.0\r\n\r\n";
-    rt = send(sock, data, sizeof(data), 0);
-    if(rt<=0)
-    {
-    	std::cout << "send() failed\n";
-    }
-    std::cout << "send success\n";
-
-    std::string buff;
-    buff.resize(4096);
-    rt = recv(sock, &buff[0], buff.size(), 0);
-    if(rt<=0)
-    {
-        std::cout << "recv() failed\n";
-    }
-    std::cout << "recv success\n";
-
-    buff.resize(10);
-    std::cout << buff << std::endl;
+    perror(msg);
+    printf("erreur...\n");
+    exit(1);
 }
 
-int main(int argc, char const *argv[])
+void watch_io_read()
 {
-	IOManager manager(2);
-	for(int i=0;i<4;i++)
-	{
-		manager.scheduleLock(&func);
-	}
-	return 0;
+    sylar::IOManager::GetThis()->addEvent(sock_listen_fd, sylar::IOManager::READ, test_accept);
+}
+
+void test_accept()
+{
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    socklen_t len = sizeof(addr);
+    int fd = accept(sock_listen_fd, (struct sockaddr *)&addr, &len);
+    if (fd < 0)
+    {
+        //std::cout << "accept failed, fd = " << fd << ", errno = " << errno << std::endl;
+    }
+    else
+    {
+        std::cout << "accepted connection, fd = " << fd << std::endl;
+        fcntl(fd, F_SETFL, O_NONBLOCK);
+        sylar::IOManager::GetThis()->addEvent(fd, sylar::IOManager::READ, [fd]()
+        {
+            char buffer[1024];
+            memset(buffer, 0, sizeof(buffer));
+            while (true)
+            {
+                int ret = recv(fd, buffer, sizeof(buffer), 0);
+                if (ret > 0)
+                {
+                    // 打印接收到的数据
+                    //std::cout << "received data, fd = " << fd << ", data = " << buffer << std::endl;
+                    
+                    // 构建HTTP响应
+                    const char *response = "HTTP/1.1 200 OK\r\n"
+                                           "Content-Type: text/plain\r\n"
+                                           "Content-Length: 13\r\n"
+                                           "Connection: keep-alive\r\n"
+                                           "\r\n"
+                                           "Hello, World!";
+                    
+                    // 发送HTTP响应
+                    ret = send(fd, response, strlen(response), 0);
+                   // std::cout << "sent data, fd = " << fd << ", ret = " << ret << std::endl;
+
+                    // 关闭连接
+                     close(fd);
+                     break;
+                }
+                if (ret <= 0)
+                {
+                    if (ret == 0 || errno != EAGAIN)
+                    {
+                        //std::cout << "closing connection, fd = " << fd << std::endl;
+                        close(fd);
+                        break;
+                    }
+                    else if (errno == EAGAIN)
+                    {
+                        //std::cout << "recv returned EAGAIN, fd = " << fd << std::endl;
+                        //std::this_thread::sleep_for(std::chrono::milliseconds(50)); // 延长睡眠时间，避免繁忙等待
+                    }
+                }
+            }
+        });
+    }
+    sylar::IOManager::GetThis()->addEvent(sock_listen_fd, sylar::IOManager::READ, test_accept);
+}
+
+void test_iomanager()
+{
+    int portno = 8080;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    // 设置套接字
+    sock_listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_listen_fd < 0)
+    {
+        error("Error creating socket..\n");
+    }
+
+    int yes = 1;
+    // 解决 "address already in use" 错误
+    setsockopt(sock_listen_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+
+    memset((char *)&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(portno);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // 绑定套接字并监听连接
+    if (bind(sock_listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+        error("Error binding socket..\n");
+
+    if (listen(sock_listen_fd, 1024) < 0)
+    {
+        error("Error listening..\n");
+    }
+
+    printf("epoll echo server listening for connections on port: %d\n", portno);
+    fcntl(sock_listen_fd, F_SETFL, O_NONBLOCK);
+    sylar::IOManager iom(9);
+    iom.addEvent(sock_listen_fd, sylar::IOManager::READ, test_accept);
+}
+
+int main(int argc, char *argv[])
+{
+    test_iomanager();
+    return 0;
 }
